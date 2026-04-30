265175f354b525aa920112b18f10234fe1ec18a6
Nievas2 <angelgabrielnievas@gmail.com>
2026-04-30 10:43:37 -0300

    style: :lipstick: add scrollbar custom

--- src/App.tsx
+++ src/App.tsx
@@ -50,15 +50,19 @@ function App() {
           </button>
         </div>
 
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 overflow-auto">
           {loading ? (
-            <>
-              {Array.from({ length: 8 }).map(() => (
-                <TaskCardSkeleton key={crypto.randomUUID()} />
-              ))}
-            </>
-          ) : (
+            Array.from({ length: 8 }).map(() => (
+              <TaskCardSkeleton key={crypto.randomUUID()} />
+            ))
+          ) : filtered.length > 0 ? (
             filtered.map((task) => <TaskCard task={task} key={task.id} />)
+          ) : (
+            <div className="w-full h-56 flex items-center justify-center">
+              <p className="text-lg text-gray-700 dark:text-gray-500 font-light">
+                No quedan tareas
+              </p>
+            </div>
           )}
         </div>
       </section>
